f4fcb5e6f6eeb3da27c5ea0ad328f9646f897cb4
Victor Hugo <64437884+ovictorhugo@users.noreply.github.com>
2024-04-20 20:27:31 -0300

    3

--- src/components/modals/confirm-delete-researcher.tsx
+++ src/components/modals/confirm-delete-researcher.tsx
@@ -1,17 +1,32 @@
-import { Dialog, DialogContent } from "@radix-ui/react-dialog";
-import { DialogHeader } from "../ui/dialog";
+
+import { DialogFooter, DialogHeader, Dialog, DialogContent, DialogDescription, DialogTitle } from "../ui/dialog";
 import { useModal } from "../hooks/use-modal-store";
+import { Button } from "../ui/button";
+import { Trash } from "phosphor-react";
 
 export function ConfirmDeleteResearcher() {
     const { onClose, isOpen, type: typeModal, data } = useModal();
     const isModalOpen = isOpen && typeModal === "confirm-delete-researcher";
 
-        const id_delete = data.id_delete
+    const id_delete = data.id_delete
+
     return(
         <Dialog open={isModalOpen} onOpenChange={onClose}> 
         <DialogContent>
         <DialogHeader className="pt-8 px-6">
+        <DialogTitle className="text-2xl text-center font-medium">
+          Cadastrar programa de <strong className="bg-blue-700 text-white hover:bg-blue-800 transition duration-500 font-medium">pós-graduação</strong>
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+          Adicione as informações básicas do programa de pós-graduação como o corpo docente envolvido, classificação e descrição.
+          </DialogDescription>
             </DialogHeader>
+
+            <DialogFooter className=" py-4 ">
+              <Button variant={'destructive'}   >
+              <Trash size={16} className="" />Deletar
+              </Button>
+            </DialogFooter>
 
             </DialogContent>
             </Dialog>
