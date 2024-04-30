0c4f4f7b9f79bee0af6c492fee1675fce36d83bb
Victor Hugo <64437884+ovictorhugo@users.noreply.github.com>
2024-04-30 13:50:29 -0300

    1.0.0

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -48,11 +48,11 @@ export function Header() {
         <header className={`h-20 z-[3] flex justify-between  items-center mr-[72px] sticky top-0  ${posGraduation == true ? ('bg-transparent'):('dark:bg-neutral-900 bg-gray-100')}`}>
             <div className="  flex items-center h-12 gap-4">
             <div className="flex gap-2 items-center h-full justify-center ">
-            <Link to={"/"} className="h-[24px]  " onClick={() => handleClick()} >{theme == 'dark' ? (<LogoWhite />):(<LogoSimcc />)}</Link>
+            <Link to={"/"} className="h-[28px]  " onClick={() => handleClick()} >{theme == 'dark' ? (<LogoWhite />):(<LogoSimcc />)}</Link>
 
             <Separator orientation="vertical" />
 
-            <Link to={""} target="_blank" className=" whitespace-nowrap "><img src={theme == 'dark' ? (logo_4_white):(logo_4)} alt="" className="whitespace-nowrap flex flex-1 h-[24px]" /></Link>
+            <Link to={""} target="_blank" className=" whitespace-nowrap "><img src={theme == 'dark' ? (logo_4_white):(logo_4)} alt="" className="whitespace-nowrap flex flex-1 h-[36px]" /></Link>
             </div>
 
             <NavigationMenu className="xl:flex hidden">
