8e096376afa023a8fa1c351c5275c463d3949a4e
rafaelnargolo <124110743+rafaelnargolo@users.noreply.github.com>
2025-01-08 12:03:58 -0300

    Responsividade página inicial quase toda, faltando uma parte do rodapé

--- src/components/homepage/components/newsletter.tsx
+++ src/components/homepage/components/newsletter.tsx
@@ -6,15 +6,15 @@ import { UserContext } from "../../../context/context";
 import { toast } from "sonner"
 
 export function Newsletter() {
-    const {version, urlGeralAdm} = useContext(UserContext)
+    const { version, urlGeralAdm } = useContext(UserContext)
 
     const [input, setInput] = useState('')
 
     const handleSubmitBolsista = async () => {
         try {
-    
+
             let urlBolsistaInsert = `${urlGeralAdm}/newsletter?email=${input}`;
-        
+
             const response = await fetch(urlBolsistaInsert, {
                 mode: 'cors',
                 method: 'POST',
@@ -25,7 +25,7 @@ export function Newsletter() {
                     'Access-Control-Max-Age': '3600'
                 },
             });
-  
+
             if (response.ok) {
                 toast("Dados enviados com sucesso", {
                     description: "Email cadastrado no newsletter",
@@ -35,10 +35,10 @@ export function Newsletter() {
                     },
                 });
             }
-  
-    
+
+
         } catch (error) {
-            
+
             toast("Erro ao processar a requisição", {
                 description: "Tente novamente mais tarde.",
                 action: {
@@ -46,33 +46,29 @@ export function Newsletter() {
                     onClick: () => console.log("Fechar"),
                 },
             });
-      
+
         }
     };
-    
-  
 
-
-
-    return(
+    return (
         <div className="w-full flex flex-col items-center py-24">
-           <div className="flex items-center flex-col justify-center mb-6">
-           <h2 className="text-2xl font-medium ">Newsletter semanal do {version ? ('Conectee'):('Iapós')}</h2>
-           <p className="max-w-[750px] text-center text-lg font-light text-foreground">Receba atualizações na sua caixa de e-mails sobre as produções mais recentes</p>
-           </div>
-
-           <div className="max-w-[540px]">
-           <div className="flex gap-3 w-full">
-           <div className="w-full flex flex-1">
-                <Input value={input} onChange={(e) => setInput(e.target.value)}></Input>
+            <div className="flex items-center flex-col justify-center mb-6">
+                <h2 className="text-2xl font-medium ">Newsletter semanal do {version ? ('Conectee') : ('Iapós')}</h2>
+                <p className="max-w-[750px] text-center text-lg font-light text-foreground">Receba atualizações na sua caixa de e-mails sobre as produções mais recentes</p>
             </div>
 
-            <Button onClick={() => handleSubmitBolsista()}><Envelope size={16}/> Fazer inscrição</Button>
-           </div>
+            <div className="max-w-[540px]">
+                <div className="flex gap-3 w-full">
+                    <div className="w-full flex flex-1">
+                        <Input value={input} onChange={(e) => setInput(e.target.value)}></Input>
+                    </div>
 
-            <p className="text-sm mt-4">Suas informações vão ser usadas de acordo com a
-Política de Privacidade do {version ? ('Conectee'):('Iapós')}. É possível cancelar a inscrição a qualquer momento.</p>
-           </div>
+                    <Button onClick={() => handleSubmitBolsista()}><Envelope size={16} /> Fazer inscrição</Button>
+                </div>
+
+                <p className="text-sm mt-4">Suas informações vão ser usadas de acordo com a
+                    Política de Privacidade do {version ? ('Conectee') : ('Iapós')}. É possível cancelar a inscrição a qualquer momento.</p>
+            </div>
 
         </div>
     )
