2403bd8a30a220f55101d77ec7b669e2df9559cb
Victor Hugo <64437884+ovictorhugo@users.noreply.github.com>
2025-01-21 12:14:33 -0300

    e.e

--- src/components/provimento-cargo/info-provimento-cargo.tsx
+++ src/components/provimento-cargo/info-provimento-cargo.tsx
@@ -1,6 +1,6 @@
 import { GraduationCap, Info, MapPin, Timer, User } from "lucide-react"
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
-import { CalendarBlank, PuzzlePiece } from "phosphor-react"
+import { CalendarBlank, PuzzlePiece, Rows, SquaresFour } from "phosphor-react"
 import { useContext, useMemo, useState } from "react"
 import { UserContext } from "../../context/context"
 import { Research } from "../researcher/researcher-page"
@@ -10,12 +10,19 @@ import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 import { CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip"
 import { TempoGrafico } from "./grafico-tempo"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "../ui/accordion"
+import { Button } from "../ui/button"
+import { Skeleton } from "../ui/skeleton"
+import { DataTable } from "../dashboard/data-table"
+import { ColumnDef } from "@tanstack/react-table"
+import { Label } from "../ui/label"
+import { Input } from "../ui/input"
 interface Props {
     name:string
     dados:Dados[]
 }
 
-interface Dados {
+export interface Dados {
   nome: string,
   cpf: string,
   classe: number,
@@ -31,6 +38,8 @@ interface Dados {
 export function InfoPavimentoCargo(props:Props) {
     const { urlGeral, user, itemsSelecionados, setSearchType, setValoresSelecionadosExport,  setItensSelecionadosPopUp, searchType, valoresSelecionadosExport, setPesquisadoresSelecionados, pesquisadoresSelecionados, setItensSelecionados, permission } = useContext(UserContext);
 
+    const [mestrado, setMestrado] = useState('')
+    const [doutorado, setDoutorado] = useState('')
     const [loading, isLoading] = useState(false)
     const [researcher, setResearcher] = useState<Research[]>([]); 
    
@@ -127,7 +136,126 @@ export function InfoPavimentoCargo(props:Props) {
           
             // Combina os dados atuais com progressões futuras
             const combinedData = [...sortedData, ...futureProgressions];
+            const items = Array.from({ length: 12 }, (_, index) => (
+              <Skeleton key={index} className="w-full rounded-md h-[300px]" />
+            ));
+
+            const [typeVisu, setTypeVisu] = useState('block');
+            const [typeVisu2, setTypeVisu2] = useState('block');
+
+
+          ////////////
+          const columns: ColumnDef<Dados>[] = [
+            {
+              accessorKey: "classe",
+              header: "Classe",
+            },
+            {
+              accessorKey: "nivel",
+              header: "Nível",
+            },
+            {
+              accessorKey: "inicio",
+              header: "Início",
+              cell: ({ row }) => new Date(row.original.inicio).toLocaleDateString("pt-BR"),
+            },
+            {
+              accessorKey: "fim",
+              header: "Fim",
+              cell: ({ row }) =>
+                row.original.fim
+                  ? new Date(row.original.fim).toLocaleDateString("pt-BR")
+                  : "N/A",
+            },
+            {
+              accessorKey: "tempo_nivel",
+              header: "Tempo no Cargo",
+              cell: ({ row }) => (row.original.tempo_nivel ? `${row.original.tempo_nivel} dias` : "N/A"),
+            },
+            {
+              accessorKey: "inicioCorreto",
+              header: "Início Correto",
+              cell: ({ row }) =>
+                row.original.inicioCorreto
+                  ? new Date(row.original.inicioCorreto).toLocaleDateString("pt-BR")
+                  : "N/A",
+            },
+            {
+              accessorKey: "fimCorreto",
+              header: "Fim Correto",
+              cell: ({ row }) =>
+                row.original.fimCorreto
+                  ? new Date(row.original.fimCorreto).toLocaleDateString("pt-BR")
+                  : "N/A",
+            },
+          ];
+
+
+          const processData = combinedData.map((item, index) => {
+            const inicioDate = new Date(item.inicio);
+            const fimDate =
+              index === sortedData.length - 1 && !item.fim
+                ? new Date(
+                    inicioDate.getTime() +
+                      (intersticio_tabela[item.classe]?.[item.nivel] ?? 0) * 24 * 60 * 60 * 1000
+                  )
+                : new Date(item.fim);
+          
+            const hasPriorValidItem = combinedData.some(
+              (prevItem, prevIndex) =>
+                prevIndex < index &&
+                prevItem.tempo_acumulado > 0 &&
+                prevItem.tempo_nivel != null
+            );
+          
+            let inicioCorreto = null;
+            let fimCorreto = null;
+          
+            if (hasPriorValidItem || (item.tempo_acumulado > 0 && item.tempo_nivel != null)) {
+              inicioCorreto = new Date(
+                index === 0
+                  ? inicioDate.getTime() + 1 * 24 * 60 * 60 * 1000
+                  : new Date(combinedData[index - 1]?.fim).getTime() + 1 * 24 * 60 * 60 * 1000
+              ).toISOString();
+          
+              fimCorreto = new Date(
+                new Date(inicioCorreto).getTime() +
+                  (intersticio_tabela[item.classe]?.[item.nivel] ?? 0) * 24 * 60 * 60 * 1000
+              ).toISOString();
+          
+              item.fimCorreto = fimCorreto;
+            }
+          
+            return { ...item, inicioCorreto, fimCorreto };
+          });
+
+
+          //////
+
+          const formatarData = (valor: string) => {
+            // Remove tudo que não seja número
+            const cleaned = valor.replace(/\D/g, "");
             
+            // Aplica a máscara de data dd/mm/aaaa
+            if (cleaned.length <= 2) {
+              return cleaned;
+            } else if (cleaned.length <= 4) {
+              return `${cleaned.slice(0, 2)}/${cleaned.slice(2)}`;
+            } else {
+              return `${cleaned.slice(0, 2)}/${cleaned.slice(2, 4)}/${cleaned.slice(4, 8)}`;
+            }
+          };
+        
+          const handleChange = (e:any) => {
+            const formattedValue = formatarData(e);
+            setDoutorado(formattedValue);
+          };
+
+          const handleChangeD = (e:any) => {
+            const formattedValue = formatarData(e);
+            setMestrado(formattedValue);
+          };
+
 return(
     <main className="flex gap-8 flex-col">
     
@@ -169,8 +297,20 @@ return(
                                     </div>
         )})}
 
-        <Alert>
+        <Alert >
+      <div className="flex gap-4 items-center">
+      <div className="flex flex-col gap-2 mt-4 w-full">
+                <Label>Conclusão do mestrado</Label>
+                <Input value={mestrado} onChange={(e) => handleChangeD(e.target.value)} type="text" />
+              
+            </div>
 
+            <div className="flex flex-col gap-2 mt-4 w-full">
+                <Label>Conclusão do doutorado</Label>
+                <Input value={doutorado} onChange={(e) => handleChange(e.target.value)} type="text" />
+              
+            </div>
+      </div>
 
         </Alert>
 
@@ -201,8 +341,48 @@ return(
                        
                           </CardContent>
                     </Alert>
-        <h2 className="text-2xl font-medium ">Progressores e acelerações</h2>
-        <ResponsiveMasonry
+      
+        <Accordion defaultValue="item-1" type="single" collapsible>
+        <AccordionItem value="item-1">
+        
+          <div className="flex mb-2">
+            <div className="flex justify-between items-center w-full">
+            <h2 className="text-2xl font-medium ">Progressores e acelerações</h2>
+            <div className="flex gap-3 mr-3">
+                <Button onClick={() => setTypeVisu('rows')}  variant={typeVisu === 'block' ? 'ghost' : 'outline'} size={'icon'}>
+                  <Rows size={16} className="whitespace-nowrap" />
+                </Button>
+                <Button onClick={() => setTypeVisu('block')} variant={typeVisu === 'block' ? 'outline' : 'ghost'}  size={'icon'}>
+                  <SquaresFour size={16} className="whitespace-nowrap" />
+                </Button>
+                </div>
+            </div>
+            <AccordionTrigger>
+  
+              </AccordionTrigger>
+          </div>
+
+          <AccordionContent>
+                {typeVisu === 'block' ? (
+                  loading ? (
+                    <ResponsiveMasonry
+                      columnsCountBreakPoints={{
+                        350: 2,
+                        750: 3,
+                        900: 4,
+                        1200:  6,
+                        1500: 6,
+                        1700: 7
+                      }}
+                    >
+                      <Masonry gutter="16px">
+                        {items.map((item, index) => (
+                          <div key={index}>{item}</div>
+                        ))}
+                      </Masonry>
+                    </ResponsiveMasonry>
+                  ) : (
+                    <ResponsiveMasonry
   columnsCountBreakPoints={{
     350: 1,
     750: 2,
@@ -357,6 +537,48 @@ return(
     })}
   </Masonry>
 </ResponsiveMasonry>
+                  )
+                ) : (
+                  loading ? (
+                    <Skeleton className="w-full rounded-md h-[400px]" />
+                  ) : (
+                  <div>
+<DataTable columns={columns} data={processData} />
+                  </div>
+                  )
+                )}
+              </AccordionContent>
+              </AccordionItem>
+          </Accordion>
+
+
+          <Accordion defaultValue="item-1" type="single" collapsible>
+        <AccordionItem value="item-1">
+        
+          <div className="flex mb-2">
+            <div className="flex justify-between items-center w-full">
+            <h2 className="text-2xl font-medium ">Valores a receber</h2>
+            <div className="flex gap-3 mr-3">
+                <Button onClick={() => setTypeVisu2('rows')}  variant={typeVisu2 === 'block' ? 'ghost' : 'outline'} size={'icon'}>
+                  <Rows size={16} className="whitespace-nowrap" />
+                </Button>
+                <Button onClick={() => setTypeVisu2('block')} variant={typeVisu2 === 'block' ? 'outline' : 'ghost'}  size={'icon'}>
+                  <SquaresFour size={16} className="whitespace-nowrap" />
+                </Button>
+                </div>
+            </div>
+            <AccordionTrigger>
+  
+              </AccordionTrigger>
+          </div>
+
+          <AccordionContent>
+
+          </AccordionContent>
+          </AccordionItem>
+          </Accordion>
+        
+      
 
 
         
